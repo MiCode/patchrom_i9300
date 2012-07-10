903df5a0f899deef67d3796b57253eaf8640ef6b
gexudong <gexudong@xiaomi.com>
2012-07-10 14:20:10 +0800

    use MiuiHome/res/xml/default_workspace.xml.part to overlay the desktop layout
    Change-Id: I2a754c2309f0ac51220b9e622166e399b91e522d

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -14,10 +14,6 @@ if [ $1 = "Mms" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
 fi
 
-if [ $1 = "MiuiHome" ];then
-	$XMLMERGYTOOL $1/res/values $2/res/values
-fi
-
 if [ $1 = "Phone" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
 fi
